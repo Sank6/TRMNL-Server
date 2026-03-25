41624959986bc2a7980ef81fd723317d2f5c8ad5
Sank6 <25014241+Sank6@users.noreply.github.com>
2026-03-25 12:47:54 +0000

    using bonjour

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { dirname } from "path";
 import { loadConfig } from "./config.js";
 import { openDatabase } from "./db/index.js";
 import { buildApp } from "./app.js";
+import Bonjour from "bonjour-service";
 
 const config = loadConfig();
 
@@ -16,6 +17,13 @@ try {
   await app.listen({ port: config.port, host: config.host });
   console.log(`xteink-server listening on ${config.host}:${config.port}`);
   console.log(`BASE_URL: ${config.baseUrl}`);
+
+  const bonjour = new Bonjour();
+  bonjour.publish({ name: "trmnl", type: "http", port: config.port, host: "trmnl.local" });
+  console.log(`mDNS: advertising as trmnl.local:${config.port}`);
+
+  process.on("SIGINT", () => { bonjour.unpublishAll(() => process.exit()); });
+  process.on("SIGTERM", () => { bonjour.unpublishAll(() => process.exit()); });
 } catch (err) {
   app.log.error(err);
   process.exit(1);
